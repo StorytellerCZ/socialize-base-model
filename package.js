375a763c7ad5a117b8d8c130fe6147c34013214a
StorytellerCZ <storyteller@freedombase.net>
2018-01-30 19:18:30 +0100

    Add missing message-box dependency

--- package.js
+++ package.js
@@ -1,44 +1,48 @@
 /* global Package, Npm */
 Package.describe({
-    name: 'socialize:base-model',
-    summary: 'A model for all other models to extend.',
-    version: '1.1.2',
-    git: 'https://github.com/copleykj/socialize-base-model.git',
+  name: 'socialize:base-model',
+  summary: 'A model for all other models to extend.',
+  version: '1.1.3',
+  git: 'https://github.com/copleykj/socialize-base-model.git'
 });
 
 Npm.depends({
-    'mongodb-diff': '0.4.4',
+  'mongodb-diff': '0.4.4',
+  'message-box': '0.2.0'
 });
 
 Package.onUse(function onUse(api) {
-    api.versionsFrom('1.3');
+  api.versionsFrom('1.3');
 
-    api.use(['meteor', 'mongo', 'ecmascript']);
+  api.use(['meteor', 'mongo', 'ecmascript']);
 
-    api.use('cultofcoders:redis-oplog', { weak: true });
+  api.use('cultofcoders:redis-oplog', { weak: true });
 
-    api.use([
-        'aldeed:collection2@3.0.0', 'aldeed:schema-index@3.0.0', 'aldeed:schema-deny@3.0.0',
-        'matb33:collection-hooks@0.8.4', 'socialize:server-time@1.0.0',
-    ]);
+  api.use([
+    'aldeed:collection2@3.0.0',
+    'aldeed:schema-index@3.0.0',
+    'aldeed:schema-deny@3.0.0',
+    'matb33:collection-hooks@0.8.4',
+    'socialize:server-time@1.0.0'
+  ]);
 
-    api.imply(['meteor', 'mongo', 'ecmascript']);
+  api.imply(['meteor', 'mongo', 'ecmascript']);
 
-    api.imply([
-        'aldeed:collection2', 'aldeed:schema-index', 'aldeed:schema-deny',
-        'matb33:collection-hooks', 'socialize:server-time',
-    ]);
+  api.imply([
+    'aldeed:collection2',
+    'aldeed:schema-index',
+    'aldeed:schema-deny',
+    'matb33:collection-hooks',
+    'socialize:server-time'
+  ]);
 
-    api.mainModule('base-model.js');
+  api.mainModule('base-model.js');
 });
 
 Package.onTest(function onTest(api) {
-    api.use(['tinytest', 'meteor', 'mongo', 'ecmascript']);
+  api.use(['tinytest', 'meteor', 'mongo', 'ecmascript']);
 
-    api.use([
-        'aldeed:collection2@3.0.0', 'matb33:collection-hooks@0.8.4',
-        'socialize:base-model',
-    ]);
+  api.use(['aldeed:collection2@3.0.0', 'matb33:collection-hooks@0.8.4', 'socialize:base-model']);
 
-    api.mainModule('tests.js');
+  api.mainModule('tests.js');
 });
